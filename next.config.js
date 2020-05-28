f6d1645ca627cffb8a4c3f89bc60dcf10618eeeb
Matt MacAulay <mac@twg.io>
2020-05-27 23:33:08 -0400

    Use file-loader for svg

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@ module.exports = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack', 'url-loader'],
+      loader: 'file-loader',
+      options: {
+        name: '[name]_[hash].[ext]',
+        publicPath: `/_next/static/files`,
+        outputPath: 'static/files'
+      }
     })
 
     return config
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,30 @@
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 import style from './index.module.css'
-import plaidshirtSvg from './plaidshirt.svg'
+import PlaidShirtSVG from './plaidshirt.svg'
 
 const DynamicFooter = dynamic(() => import('./footer'))
 
 export default function Home() {
   return (
-    <div className={style.container}>
+    <>
       <Head>
         <title>Plaid Shirt</title>
-        <link rel="icon" href={plaidshirtSvg} />
+        <link rel="icon" href={PlaidShirtSVG} />
       </Head>
 
-      <header className={style.tartanHeader}></header>
+      <div className={style.container}>
+        <header className={style.tartanHeader}></header>
 
-      <main>
-        <h1 className={style.title}>
-          Plaid Shirt
-          <img className={style.shirt} src={plaidshirtSvg} alt="Plaid Shirt" />
-        </h1>
-      </main>
+        <main>
+          <h1 className={style.title}>
+            Plaid Shirt
+            <img className={style.shirt} src={PlaidShirtSVG} alt="Plaid Shirt" />
+          </h1>
+        </main>
 
-      <DynamicFooter />
-    </div>
+        <DynamicFooter />
+      </div>
+    </>
   )
 }
